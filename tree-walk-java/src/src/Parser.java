fc6c40573cce6fcb6fc7a150a800c9e43bac3ef2
vektor8 <victor.mosolea@proton.me>
2023-11-01 13:44:39 +0200

    comma operator between expressions

--- tree-walk-java/src/src/Parser.java
+++ tree-walk-java/src/src/Parser.java
@@ -14,7 +14,14 @@ class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        // expr -> equality ("," equality)*
+        Expr expr = equality();
+        while (match(COMMA)){
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
     }
 
     private Expr equality() {
